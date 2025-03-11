866ec5af776a8046c3b2bd6eb79e6b48eee2ad7f
verysonglaa <graf.gabriel@gmail.com>
2025-03-11 14:58:23 +0100

    improve deploy script

--- deliver/deploy/deploy.sh
+++ deliver/deploy/deploy.sh
@@ -6,13 +6,7 @@ if [[ "$confirmation" != "y" && "$confirmation" != "Y" ]]; then
     exit 1
 fi
 
-terraform apply -auto-approve --var-file prod.tfvars
-terraform output --raw training-kubeconfig > ~/.kube/training-cluster-config
-echo
-echo "export KUBECONFIG=~/.kube/training-cluster-config"
-echo "kubectl -n welcome port-forward services/welcome 8080:80 &"
-sleep 15
-echo "curl http://localhost:8080/teacher"
+
 
 for i in {0..3}; do
 
@@ -21,3 +15,16 @@ for i in {0..3}; do
     terraform output -json student-vm-ssh-keys | jq -r .[$i].private_key_openssh 
     echo "ssh -i $user.pem $user@$(terraform output -json student-vm-ips | jq -r .[$i]) "
 done
+
+
+terraform apply -auto-approve --var-file prod.tfvars
+terraform output --raw training-kubeconfig > ~/.kube/training-cluster-config
+echo
+echo "export KUBECONFIG=~/.kube/training-cluster-config"
+echo "kubectl -n welcome port-forward services/welcome 8080:80 &"
+sleep 15
+echo "curl http://localhost:8080/teacher"
+echo
+echo "access argocd under https://argocd.training.cluster.songlaa.com"
+echo "username: admin"
+echo "password: $(terraform output argocd-admin-password)"
